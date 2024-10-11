aa0b8b6ad3f8725666cd91c796c57a9a8edd867a
Andrew Keenan Richardson <richardson.andrew@gmail.com>
2024-10-11 00:09:21 -0700

    feat: Remove dinner_party.py and update run_evaluation.sh

--- lm_eval/tasks/dinner_party/run_evaluation.sh
+++ lm_eval/tasks/dinner_party/run_evaluation.sh
@@ -2,7 +2,7 @@
 
 lm_eval --model openai-chat-completions \
     --model_args model=gpt-4 \
-    --tasks dinner_party \
+    --tasks generation/tasks/dinner_party/dinner_party.yaml \
     --num_fewshot 0 \
     --batch_size 1 \
     --output_path lm_eval/tasks/dinner_party/results \

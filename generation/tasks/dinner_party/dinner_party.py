10c06c7cbeea0c36f7c56c9e63613260c3367fc5
Andrew Keenan Richardson <richardson.andrew@gmail.com>
2024-10-21 13:56:37 -0700

    Remove aider's wild fixing

--- generation/tasks/dinner_party/dinner_party.py
+++ generation/tasks/dinner_party/dinner_party.py
@@ -214,15 +214,6 @@ class DinnerParty(TaskSpecification):
             for idx in discounted_indices:
                 points_per_person[idx] = max(1, points_per_person[idx] - bimodal_discount)
         
-        # Adjust all points proportionally to sum up to total_points
-        current_total = sum(points_per_person)
-        if current_total != total_points:
-            scaling_factor = total_points / current_total
-            points_per_person = [max(1, int(points * scaling_factor)) for points in points_per_person]
-
-        # Adjust the last person's points to make the total exactly match total_points
-        points_per_person[-1] += total_points - sum(points_per_person)
-        
         people = [Person.random_person(name, selected_interests, points, min_interests, max_interests) 
                   for name, points in zip(selected_names, points_per_person)]
         task_description = f"Select {set_size} people for a dinner party that will have the most engaging conversations."

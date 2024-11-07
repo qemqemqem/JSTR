318d707d8aa7da9d0fdb60f9a8efce0c18c8048d
Andrew Keenan Richardson <richardson.andrew@gmail.com>
2024-11-07 08:53:16 -0800

    docs: Add docstring explaining random scoring rules for dinner party game

--- generation/tasks/dinner_party/random_scoring_rules.py
+++ generation/tasks/dinner_party/random_scoring_rules.py
@@ -1,4 +1,30 @@
+"""
+A utility for generating random scoring rules for a dinner party game.
+
+In this game, the player must choose 5 dinner party guests from a list of 10. Each guest has some set of interests, like a level 4 interest in cooking, or a level 7 interest in gardening.
+
+The scoring for this game can be randomized, and this file contains utilities for generating random scoring rules.
+
+Typically, scoring rules are applied in multiple rounds of scoring. Different scoring rules can be more or less complex.
+
+Each rule has some selection mechanism for choosing which interests are involved in the scoring. A simple rule might be to always choose sports and cooking, while a more complex rule might choose the top 3 interests of each guest.
+
+Each rule has some scoring mechanism for determining how many points a guest gets. A simple rule might be to add up the interest values for the chosen interest, while a more complex rule might give 1 point for each interest, plus 1 point for each interest that is shared with the host.
+
+Here are some examples of scoring rules:
+
+- (CR1) Each guest gets their top interest value.
+- (CR1) Each guest gets their value in cooking interest.
+- (CR2) Each guest gets the sum of their top 3 interests.
+- (CR2) Each guest gets their second-highest interest value.
+- (CR3) The most common interest is chosen among all guests, and each guest gets their value in that interest.
+- (CR3) The largest interest value is chosen among all guests, and each guest gets their value in that interest.
+- (CR4) The host is chosen as the alphabetically lowest guest, and each guest gets 2 points for each interest they share with the host.
+- (CR4) The most common interest is chosen among all guests, excluding interests which have been chosen in previous rounds, and each guest gets their value in that interest. (Round 2 and later only)
+"""
+
 import random
+
 
 
 def random_scoring_rules(points: int):

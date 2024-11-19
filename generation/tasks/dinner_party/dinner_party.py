76087cac07676dcd055d494d23500bb8d50ad78a
Andrew Keenan Richardson <richardson.andrew@gmail.com>
2024-11-18 17:46:37 -0800

    Load from dict

--- generation/tasks/dinner_party/dinner_party.py
+++ generation/tasks/dinner_party/dinner_party.py
@@ -373,11 +373,9 @@ class DinnerParty(TaskSpecification):
         
         # Load scoring rules if they exist in the data
         if 'scoring_rules' in data and data['scoring_rules'] is not None:
-            print("Attempting to load scoring rules")
             print(data['scoring_rules'])
-            rules = [scoring_rule_from_dict(rule_data) for rule_data in data['scoring_rules']]
-            dinner_party.random_scoring_rules = GameScoring(target_complexity=len(rules), rules=rules)
-            print(f"Loaded scoring rules: {dinner_party.random_scoring_rules}")
+            dinner_party.random_scoring_rules = GameScoring.from_dict(data["scoring_rules"], dinner_party=dinner_party)
+            # print(f"Loaded scoring rules: {dinner_party.random_scoring_rules}")
         
         return dinner_party
 
